d3566c934fa5012ed8cc7c28862ba0c62324958c
leandro160320 <148804278+leandro160320@users.noreply.github.com>
2024-04-18 18:31:06 +0000

    feat: Header e Banner

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -1,7 +1,7 @@
 
 export default function Banner(){
     return(
-        <div className="text-center text-white bg-[#FF6403] py-20 mx-2">
+        <div className="text-center text-white bg-[#FF6403] py-20 mx-4">
                 <div className="mb-8">Let's go now</div>
                 <div className="text-7xl"> explore end travel</div>
                     <button className="bg-[#74CC01] rounded px-10 py-2 text-xs mt-10">
