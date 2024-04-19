9de1b5e01e3f725dfa8a81addc00fb1ef08f10bc
leandro160320 <148804278+leandro160320@users.noreply.github.com>
2024-04-19 04:33:11 +0000

    feat: Heading ok

--- app/components/Heading.tsx
+++ app/components/Heading.tsx
@@ -1,6 +1,59 @@
 
 export default function Heading(){
     return(
-        <h1>ola</h1>
+        <div className=" text-center mb-20">
+          <div className="text-[#FF6403] m-5 mt-20">
+            <h1>Our Presence</h1>
+          </div>
+          <div className="text-5xl font-bold my-5">
+            <h1>Contact Us</h1>
+
+            <div className="border-b-4 border-[#FF6403] my-6 mx-40"></div>
+          </div>
+          <div className="flex justify-center">
+          <a className="mr-2" href="">
+              <svg
+                className="w-6 h-6 dark"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M13.135 6H15V3h-1.865a4.147 4.147 0 0 0-4.142 4.142V9H7v3h2v9.938h3V12h2.021l.592-3H12V6.591A.6.6 0 0 1 12.592 6h.543Z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+              </a>
+
+              <a class="mr-2" href="">
+                <svg
+                  class="w-6 h-6 dark"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M22 5.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.343 8.343 0 0 1-2.605.981A4.13 4.13 0 0 0 15.85 4a4.068 4.068 0 0 0-4.1 4.038c0 .31.035.618.105.919A11.705 11.705 0 0 1 3.4 4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 6.1 13.635a4.192 4.192 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 2 18.184 11.732 11.732 0 0 0 8.291 20 11.502 11.502 0 0 0 19.964 8.5c0-.177 0-.349-.012-.523A8.143 8.143 0 0 0 22 5.892Z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </a>
+          </div>
+          
+          <div className="my-5 text-[#333333]">
+            <h1>
+              Travel makes one modest. You see what a tiny place you occupy in the world.
+            </h1>
+          </div>
+        </div>
+    
     )
 }
