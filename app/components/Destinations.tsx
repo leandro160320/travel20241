61f73a10745d791c21ca88f6e4f0c36d87e7d837
leandro160320 <148804278+leandro160320@users.noreply.github.com>
2024-04-19 03:34:22 +0000

    feat: Destinations ok

---
+++ app/components/Destinations.tsx
@@ -0,0 +1,52 @@
+
+export default function Destinations(){
+    return(
+        <div className="text-center my-20 justifay-between">
+            <h1 className="text-[#FF6403]">Choose Your</h1>
+            <h1 className="text-7xl my-8 font-bold">Perfect Holiday</h1>
+            <div className="border-b-4 border-[#FF6403] my-6 mx-40"></div>
+            <h1 className="mb-16">The most searched holiday destinations.</h1>
+            <div className="mx-4" >
+                <div>
+                     <img src="/img/holiday-1.jpg.png" alt="" />
+                     <h1 className="my-2 font-bold">Switzerland - 7 Days</h1>
+                     <h1 className="text-[#FF6403]">$270</h1>
+                </div>
+
+                <div>
+                     <img src="/img/holiday-2.jpg.png" alt="" />
+                     <h1 className="my-2 font-bold">St. Mortiz Ski Package</h1>
+                     <h1 className="text-[#FF6403]">$450</h1>
+                </div>
+
+                <div>
+                     <img src="/img/holiday-3.jpg.png" alt="" />
+                     <h1 className="my-2 font-bold">Big in Japan</h1>
+                     <h1 className="text-[#FF6403]">$522</h1>
+                </div>
+
+                <div>
+                     <img src="/img/holiday-4.jpg.png" alt="" />
+                     <h1 className="my-2 font-bold">Japan Subway Ticket</h1>
+                     <h1 className="text-[#FF6403]">$255</h1>
+                </div>
+
+                <div>
+                     <img src="/img/holiday-5.jpg.png" alt="" />
+                     <h1 className="my-2 font-bold">Helicopter Flight</h1>
+                     <h1 className="text-[#FF6403]">$250</h1>
+                </div>
+
+                <div>
+                     <img src="/img/holiday-6.jpg.png" alt="" />
+                     <h1 className="my-2 font-bold">Snorkeling Adventure</h1>
+                     <h1 className="text-[#FF6403]">$412</h1>
+                </div>
+                <button className="bg-[#74CC01] text-white rounded px-10 py-2 text-xs mt-10">
+                     VIEW ALL
+                    </button>
+                 
+            </div>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Header from "./components/Header";
 import Banner from "./components/Banner";
 import Section from "./components/Section";
+import Destinations from "./components/Destinations";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
        <Header/>
        <Banner/>
        <Section/>
+       <Destinations/>
     </div>
     
    
